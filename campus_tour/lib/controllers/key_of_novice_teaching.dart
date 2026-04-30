ae44b1306de83358a45c7c25dbc961273cb4cb79
albertLHF <rikuhonnfuu@gmail.com>
2026-04-30 22:12:45 +0800

    教學還沒寫，但先把key定義清楚

---
+++ campus_tour/lib/controllers/key_of_novice_teaching.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class KeyOfNoviceTeaching {
+  static final GlobalKey mainCharacter = GlobalKey();
+  static final GlobalKey avatar = GlobalKey();
+  static final GlobalKey avatarImage = GlobalKey();
+  static final GlobalKey avatarCheak = GlobalKey();
+  static final GlobalKey arrow = GlobalKey();
+  static final GlobalKey encyclopediaButton = GlobalKey();
+  static final GlobalKey encyclopediaCard = GlobalKey();
+  static final GlobalKey encyclopediaText = GlobalKey();
+}
