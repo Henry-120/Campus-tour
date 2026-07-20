7d82b2634fc8cdfcd24cb1a405733c00087849c7
albertLHF <rikuhonnfuu@gmail.com>
2026-07-20 11:37:43 +0800

    加入影片未測試

--- campus_tour/lib/widgets/game/catching_pages/catching_faild.dart
+++ campus_tour/lib/widgets/game/catching_pages/catching_faild.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+
 import '../../../styles/app_theme.dart';
 
-import 'package:get/get.dart';
+class CatchingFaildPage extends StatefulWidget {
+  const CatchingFaildPage({super.key});
 
-class CatchingFaildPage extends StatelessWidget {
-  CatchingFaildPage({super.key});
+  @override
+  State<CatchingFaildPage> createState() => _CatchingFaildPageState();
+}
+
+class _CatchingFaildPageState extends State<CatchingFaildPage> {
+  late final VideoPlayerController _videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoController = VideoPlayerController.asset(
+      'assets/video/faiedl_anime.mp4',
+    );
+    _initializeVideo();
+  }
+
+  Future<void> _initializeVideo() async {
+    await _videoController.initialize();
+    await _videoController.setLooping(true);
+    await _videoController.setVolume(1);
+    await _videoController.play();
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +50,24 @@ class CatchingFaildPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.sentiment_dissatisfied,
-                  size: 120,
-                  color: Colors.grey,
-                ),
-                SizedBox(height: 24),
+                if (_videoController.value.isInitialized)
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 320,
+                      maxHeight: 260,
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: _videoController.value.aspectRatio,
+                      child: VideoPlayer(_videoController),
+                    ),
+                  )
+                else
+                  const SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: Center(child: CircularProgressIndicator()),
+                  ),
+                const SizedBox(height: 24),
                 Text(
                   'widgets.game.catching.pages.catching.faild.s001'.tr,
                   style: AppTheme.titleStyle.copyWith(
@@ -30,7 +76,7 @@ class CatchingFaildPage extends StatelessWidget {
                     letterSpacing: 0,
                   ),
                 ),
-                SizedBox(height: 12),
+                const SizedBox(height: 12),
                 Text(
                   'widgets.game.catching.pages.catching.faild.s002'.tr,
                   style: AppTheme.titleStyle.copyWith(
@@ -39,7 +85,7 @@ class CatchingFaildPage extends StatelessWidget {
                     letterSpacing: 0,
                   ),
                 ),
-                SizedBox(height: 32),
+                const SizedBox(height: 32),
                 FilledButton(
                   onPressed: () {
                     Navigator.of(context).pop();
